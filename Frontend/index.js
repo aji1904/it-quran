8611f696d27ab36ddf29ac9619f072570a0ba21a
aji1904 <aji19980419@gmail.com>
2019-12-08 21:11:45 +0700

    update readme

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,22 +1,36 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-
-class HomeScreen extends React.Component {
+import React, { Component } from "react";
+import { Container, Header, Button, Content, ActionSheet, Text } from "native-base";
+var BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
+var DESTRUCTIVE_INDEX = 3;
+var CANCEL_INDEX = 4;
+export default class ActionSheetExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-      </View>
+      <Container>
+        <Header />
+        <Content padder>
+          <Button
+            onPress={() =>
+            ActionSheet.show(
+              {
+                options: BUTTONS,
+                cancelButtonIndex: CANCEL_INDEX,
+                destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                title: "Testing ActionSheet"
+              },
+              buttonIndex => {
+                this.setState({ clicked: BUTTONS[buttonIndex] });
+              }
+            )}
+          >
+            <Text>Actionsheet</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
-  },
-});
-
-export default createAppContainer(AppNavigator);
---
+++ README.md
@@ -0,0 +1,9 @@
+# it-quran
+
+# npm install react-navigation
+# npm install --save react-native-gesture-handler
+# npm install --save react-native-reanimated
+# npm install --save react-native-screens
+# npm i react-navigation-stack
+# npm install native-base --save
+# npm i react-native-actionsheet
